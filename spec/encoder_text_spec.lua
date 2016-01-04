f828b838e38d43d9c73cf2a0116218d0affd5c2c
Timur Batyrshin <erthad@gmail.com>
2016-01-04 17:22:28 +0300

    added encoder_text with a test from heka upstream

---
+++ spec/encoder_text_spec.lua
@@ -0,0 +1,13 @@
+require "spec/test_helper"
+
+describe("encoder_text", function()
+  it("processes message", function ()
+    reset_all() -- run this before every test
+
+    dofile "encoder_text.lua"       -- don't use "require", use "dofile"
+    mock_read_message({Payload="original"})
+
+    process_message()
+    assert.is.equal('{Payload=\'Prefixed original\',Fields[payload_type]=\'txt\',Fields[payload_name]=\'\'}', injected())
+  end)
+end)
