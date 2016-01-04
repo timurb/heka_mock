20a56e619ff403ddeea36daa0f6db870ed1878cd
Timur Batyrshin <erthad@gmail.com>
2016-01-04 19:29:30 +0300

    added some kind of json encoder from upstream and a test for it

---
+++ spec/encoder_json_spec.lua
@@ -0,0 +1,36 @@
+require "spec/test_helper"
+
+_G.cjson = require "cjson"
+
+describe("encoder_json", function()
+  it("processes message", function ()
+    reset_all() -- run this before every test
+
+    dofile "encoder_json.lua"       -- don't use "require", use "dofile"
+    mock_read_message({
+      Payload="original",
+      Type="my_type",
+      Pid=12345,
+      Severity=4,
+      Timestamp=54321
+    })
+
+    process_message()
+    code = "return " .. injected()
+    result = assert(loadstring(code))()
+    assert.is.table(result)
+    assert.is.equal("json", result.Fields__payload_type)
+    assert.is.equal("message_table", result.Fields__payload_name)
+
+    payload = result.Payload
+    assert.is.string(payload)
+
+    payload = cjson.decode(payload)
+    assert.is.table(payload)
+    assert.is.equal("original", payload.Payload)
+    assert.is.equal("my_type", payload.Type)
+    assert.is.equal(12345, payload.Pid)
+    assert.is.equal(4, payload.Severity)
+    assert.is.equal(54321, payload.Timestamp)
+  end)
+end)
